a9286aa7af158587b90d8dd6e0d4a90ad4fea4e1
Antonio <antonio.root@gmail.com>
2014-11-30 13:55:51 +0100

    :ballot_box_with_check: NOWPLAYING KAPPA

--- app/app.js
+++ app/app.js
@@ -5,6 +5,18 @@ var express = require('express'),
 
 var app = express();
 var server = http.createServer(app);
+var playing = 0; // U PLAYIN OR NOT BRUH
+var nowPlaying; // WOT R U PLAYIN M9 
+
+function killMplayer(){
+	console.log("Killing all mplayer processes...");
+	exec(['killall', 'mplayer'], function(err, out, code) {
+		if(err instanceof Error)
+			throw err;
+		process.stderr.write(err);
+		process.stdout.write(out);
+	});
+}
 
 app.get('/PiTube/player/:id', function(req, res){
 	var fullUrl = req.protocol + "://" + req.get('host') + req.originalUrl;
@@ -12,25 +24,39 @@ app.get('/PiTube/player/:id', function(req, res){
 	res.setHeader('Content-Type', 'text/plain');
 	res.end('Playing ' + req.params.id + ' on your raspberry Pi');
 	console.log("Now playing "+ req.params.id);
+	if(playing){
+		killMplayer();
+	}else{
+		playing = 1;
+	}
 	exec(['./ytb-player.sh', req.params.id], function(err, out, code) {
 		if(err instanceof Error)
 			throw err;
 		process.stderr.write(err);
 		process.stdout.write(out);
 	});
+	nowPlaying = req.params.id;
 });
 
 app.get('/PiTube/Killall', function(req, res){
 	var fullUrl = req.protocol + "://" + req.get('host') + req.originalUrl;
 	console.log("GET "+fullUrl)
 	res.setHeader('Content-type', 'text/plain');
-	res.end('Killed all mplayer processes');
-	exec(['killall', 'mplayer'], function(err, out, code) {
-		if(err instanceof Error)
-			throw err;
-		process.stderr.write(err);
-		process.stdout.write(out);
-	});
+	if(playing){
+		killMplayer();
+		res.end('Killed all mplayer processes');
+		playing = 0;
+	}
+});
+
+app.get('/PiTube/Nowplaying', function(req, res) {
+	res.setHeader('Content-type', 'text/plain');
+	if(nowPlaying.length > 0 ){
+		res.end(nowPlaying);
+	}else{
+		res.end("null");
+	}
 });
 
 app.listen(port);
+console.log("App running on port " + port);
