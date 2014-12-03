d4110c474d512f320bf03b6feaf5fe59597b08ea
Macak47 <antonio.root@gmail.com>
2014-12-03 01:21:29 +0100

    Installer.sh updated :tada:

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
-curl https://yt-dl.org/downloads/2014.12.01/youtube-dl -o /usr/local/bin/youtube-dl
-chmod a+x /usr/local/bin/youtube-dl
+sudo curl https://yt-dl.org/downloads/2014.12.01/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+x /usr/local/bin/youtube-dl
 cd ./app
 mkdir cache
 npm install express
 npm install youtube-feeds
 npm install exec
 npm install ejs
-chmod a+x ytb-player.sh clearcache.js app.js
+sudo chmod a+x ytb-player.sh clearcache.sh app.js
