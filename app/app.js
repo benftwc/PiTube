937f1825e5a84d53ee94cdeb9b1f07b3896960b2
Macak47 <antonio.root@gmail.com>
2014-12-08 16:33:04 +0100

    much fix such stuffs very js wow

--- app/app.js
+++ app/app.js
@@ -10,22 +10,21 @@ var express = require('express'),
 
 var app = express();
 var server = http.createServer(app);
-var playing = 0; // U PLAYIN OR NOT BRUH
-var nowPlaying; // WOT R U PLAYIN M9 
+var nowPlaying; // WOT R U PLAYIN M8
 var musics = getMusics();
 var apiPath = '/PiTube/api/';
-var video;
+var cachePath = __dirname+'/cache/';
 var player = new Mplayer();
+var paused = 0;
 
 youtube.httpProtocol = 'https';
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+'/public'));
 app.set('view engine', 'ejs');
 
 function getMusics(){
 	var musics = fs.readdirSync('./cache/');
 	for(var i=0; i<musics.length; i++){
 		musics[i] = replaceAll(".mp4", "", musics[i]);
-		musics[i] = replaceAll(",", " ", musics[i]);
 	}
 	return musics
 }
@@ -34,20 +33,14 @@ function replaceAll(find, replace, str){
 	return str.replace(new RegExp(find, 'g'), replace);
 }
 
-function killMplayer(){
-	console.log("Killing all mplayer processes...");
-	exec(['killall', 'mplayer'], function(err, out, code) {
-		if(err instanceof Error)
-			throw err;
-		process.stderr.write(err);
-		process.stdout.write(out);
-	});
-}
-
 function logfullurl(req){
 	var fullUrl = req.protocol + "://" + req.get('host') + req.originalUrl;
 	console.log("GET "+fullUrl);
 }
+
+/**
+Web app
+**/
 
 app.get('/', function(req, res){
 	logfullurl(req);
@@ -60,53 +53,79 @@ app.get('/', function(req, res){
 	});
 });
 
-app.get(apiPath+':id', function(req, res){
-	logfullurl(req);
-	youtube.feeds.videos({q:req.params.id, 'max-results': 1}, function(err, data) {
-		if( err instanceof Error){
-			console.log(err);
-		} else{
-			nowPlaying = data.items[0].title;
-			console.log(data.items[0].title);
-			nowPlaying = nowPlaying.toLowerCase();
-			fs.exists(__dirname+'/cache/'+nowPlaying+'.mp4', function(exists){
-				if(exists){
-					console.log('Playing ' + nowPlaying);
-					player.stop();
-					player.setFile(__dirname+'/cache/'+nowPlaying+'.mp4');
-					player.play();
-				}else{
-					video = youtubedl('https://www.youtube.com/watch?v='+data.items[0].id,
-						['--max-quality=18'],
-						{cwd: __dirname});
-					video.on('info', function(info){
-						console.log('Download started');
-					});
-					var f = fs.createWriteStream(__dirname+'/cache/'+nowPlaying+'.mp4');
-					f.on('finish', function(){
-						player.stop();
-						player.setFile(__dirname+'/cache/'+nowPlaying+'.mp4');
-						player.play();
-					});
-					video.pipe(f);
-				}
-			});
-		}
-		res.setHeader('Content-type', 'text/plain');
-		res.end('Playing ' + nowPlaying + ' on your raspberry Pi');
-	});
-	nowPlaying = req.params.id;
-});
+/**
+Controls
+**/
 
-app.get('/Killall', function(req, res){
+app.get('/PiTube/pause', function(req, res){
 	logfullurl(req);
+	player.pause();
 	res.setHeader('Content-type', 'text/plain');
-	if(playing){
-		killMplayer();
-		res.end('Killed all mplayer processes');
-		playing = 0;
+	if(paused){
+		res.end('Player resumed');
+		paused = 0;
+	}else{
+		res.end('Player paused');
+		paused = 1;
 	}
 });
+
+app.get('/PiTube/stop', function(req, res){
+	logfullurl(req);
+	player.stop();
+	res.setHeader('Content-type', 'text/plain');
+	res.end('Player stopped');
+});
+
+app.get(apiPath+':id', function(req, res){
+	logfullurl(req);
+	fs.exists(cachePath+req.params.id+'.mp4', function(exists){
+		if(exists){
+			nowPlaying = req.params.id.toLowerCase();
+			player.stop();
+			player.setFile(cachePath+nowPlaying+'.mp4');
+			player.play();
+		}else{
+			youtube.feeds.videos({q:req.params.id, 'max-results': 1}, function(err, data) {
+			if( err instanceof Error){
+				console.log(err);
+			} else{
+				nowPlaying = data.items[0].title;
+				console.log(data.items[0].title);
+				nowPlaying = nowPlaying.toLowerCase();
+				fs.exists(cachePath+nowPlaying+'.mp4', function(exists){
+					if(exists){
+						console.log('Playing ' + nowPlaying);
+						player.stop();
+						player.setFile(cachePath+nowPlaying+'.mp4');
+						player.play();
+					}else{
+						video = youtubedl('https://www.youtube.com/watch?v='+data.items[0].id,
+							['--max-quality=18'],
+							{cwd: __dirname});
+						video.on('info', function(info){
+							console.log('Download started');
+						});
+						var f = fs.createWriteStream(cachePath+nowPlaying+'.mp4');
+						f.on('finish', function(){
+							player.stop();
+							player.setFile(cachePath+nowPlaying+'.mp4');
+							player.play();
+						});
+						video.pipe(f);
+					}
+				});
+			}
+			res.setHeader('Content-type', 'text/plain');
+			res.end('Playing ' + nowPlaying + ' on your raspberry Pi');
+		});
+		}
+	});
+});
+
+/**
+Stuffs
+**/
 
 app.get('/Nowplaying', function(req, res) {
 	logfullurl(req);
@@ -122,7 +141,7 @@ app.get('/clearcache', function(req, res) {
 	logfullurl(req);
 	console.log("Clearing cache...");
 	fs.readdirSync(__dirname+'/cache').forEach(function(filename) {
-		fs.unlinkSync(__dirname+'/cache/'+filename);
+		fs.unlinkSync(cachePath+filename);
 	});
 	res.setHeader('Content-type', 'text/plain');
 	res.end("Cache cleared");
--- installer.sh
+++
@@ -1,6 +0,0 @@
-sudo curl https://yt-dl.org/downloads/2014.12.01/youtube-dl -o /usr/local/bin/youtube-dl
-sudo chmod a+x /usr/local/bin/youtube-dl
-cd ./app
-mkdir cache
-npm install 
-sudo chmod a+x ytb-player.sh clearcache.sh app.js
