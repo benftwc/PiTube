24fdaeb02e2f7f9ad1f9e418cfd9c2c20b85aa87
Macak47 <antonio.root@gmail.com>
2014-12-11 12:13:06 +0100

    routes.js OP

--- app/app.js
+++ app/app.js
@@ -6,28 +6,31 @@ var express = require('express'),
 	ip = require('ip'),
 	youtube = require('youtube-feeds'),
 	youtubedl = require('youtube-dl'),
-	Mplayer = require('node-mplayer');
+	Mplayer = require('node-mplayer'),
+	routes = require('./routes.js');
 
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
-var nowPlaying; // WOT R U PLAYIN M8
-var musics = getMusics();
-var apiPath = '/PiTube/api/';
+var nowPlaying = ""; // WOT R U PLAYIN M8
+var musics = getMusics(); // GIMME THOSE CACHED FILES
+var apiPath = '/PiTube/api/'; // dat path doe 
 var cachePath = __dirname+'/cache/';
 var player = new Mplayer();
-var paused = 0;
+var paused = 0; // IS DA PLAYER PAUSED
+var muted = 0; // IS DA PLAYER MUTED
 
 youtube.httpProtocol = 'https';
 app.use(express.static(__dirname+'/public'));
 app.set('view engine', 'ejs');
+
 
 function getMusics(){
 	var musics = fs.readdirSync('./cache/');
 	for(var i=0; i<musics.length; i++){
 		musics[i] = replaceAll(".mp4", "", musics[i]);
 	}
-	return musics
+	return musics;
 }
 
 function replaceAll(find, replace, str){
@@ -42,44 +45,23 @@ function logfullurl(req){
 io.sockets.on('connection', function(socket){
     console.log('New client connected');
 });
+
 /**
-Web app
+Web app routes
 **/
 
-app.get('/', function(req, res){
-	logfullurl(req);
-	musics = getMusics();
-	res.render('pages/index', {
-		nowPlaying: nowPlaying,
-		musics: musics,
-		apiPath: apiPath,
-		ip: ip.address()
-	});
-});
+app.get('/', routes.webApp(nowPlaying, musics, apiPath, ip.address()));
+app.get('/PiTube', routes.webApp(nowPlaying, musics, apiPath, ip.address()));
 
 /**
 Controls
 **/
 
-app.get('/PiTube/pause', function(req, res){
-	logfullurl(req);
-	player.pause();
-	res.setHeader('Content-type', 'text/plain');
-	if(paused){
-		res.end('Player resumed');
-		paused = 0;
-	}else{
-		res.end('Player paused');
-		paused = 1;
-	}
-});
+app.get('/PiTube/pause', routes.pause(player, paused));
 
-app.get('/PiTube/stop', function(req, res){
-	logfullurl(req);
-	player.stop();
-	res.setHeader('Content-type', 'text/plain');
-	res.end('Player stopped');
-});
+app.get('/PiTube/stop', routes.stop(player));
+
+app.get('/PiTube/mute', routes.mute(player, muted));
 
 app.get(apiPath+':id', function(req, res){
 	logfullurl(req);
@@ -89,43 +71,47 @@ app.get(apiPath+':id', function(req, res){
 			player.stop();
 			player.setFile(cachePath+nowPlaying+'.mp4');
 			player.play();
+			io.sockets.emit('nowPlaying', nowPlaying);
 		}else{
 			youtube.feeds.videos({q:req.params.id, 'max-results': 1}, function(err, data) {
-			if( err instanceof Error){
-				console.log(err);
-			} else{
-				nowPlaying = data.items[0].title;
-				console.log(data.items[0].title);
-				nowPlaying = nowPlaying.toLowerCase();
-				fs.exists(cachePath+nowPlaying+'.mp4', function(exists){
-					if(exists){
-						console.log('Playing ' + nowPlaying);
-						player.stop();
-						player.setFile(cachePath+nowPlaying+'.mp4');
-						player.play();
-						io.sockets.emit('nowPlaying', nowPlaying);
-					}else{
-						video = youtubedl('https://www.youtube.com/watch?v='+data.items[0].id,
-							['--max-quality=18'],
-							{cwd: __dirname});
-						video.on('info', function(info){
-							console.log('Download started');
-						});
-						var f = fs.createWriteStream(cachePath+nowPlaying+'.mp4');
-						f.on('finish', function(){
+				if( err instanceof Error){
+					console.log(err);
+				} else{
+					nowPlaying = data.items[0].title;
+					console.log(data.items[0].title);
+					nowPlaying = nowPlaying.toLowerCase();
+					fs.exists(cachePath+nowPlaying+'.mp4', function(exists){
+						if(exists){
+							console.log('Playing ' + nowPlaying);
 							player.stop();
 							player.setFile(cachePath+nowPlaying+'.mp4');
 							player.play();
 							io.sockets.emit('nowPlaying', nowPlaying);
-							io.sockets.emit('newFile', nowPlaying);
-						});
-						video.pipe(f);
-					}
-				});
-			}
-			res.setHeader('Content-type', 'text/plain');
-			res.end('Playing ' + nowPlaying + ' on your raspberry Pi');
-		});
+						}else{
+							video = youtubedl('https://www.youtube.com/watch?v='+data.items[0].id,
+								['--max-quality=18'],
+								{cwd: __dirname});
+							video.on('info', function(info){
+								console.log('Download started');
+							});
+							var f = fs.createWriteStream(cachePath+nowPlaying+'.mp4');
+							f.on('finish', function(){
+								player.stop();
+								player.setFile(cachePath+nowPlaying+'.mp4');
+								player.play();
+								io.sockets.emit('nowPlaying', nowPlaying);
+								io.sockets.emit('newFile', nowPlaying);
+							});
+							f.on("error", function(error){
+								console.log(error);
+							});
+							video.pipe(f);
+						}
+					});
+				}
+				res.setHeader('Content-type', 'text/plain');
+				res.end('Playing ' + nowPlaying + ' on your raspberry Pi');
+			});
 		}
 	});
 });
@@ -133,15 +119,13 @@ app.get(apiPath+':id', function(req, res){
 /**
 Stuffs
 **/
-
-app.get('/Nowplaying', function(req, res) {
-	logfullurl(req);
-	res.setHeader('Content-type', 'text/plain');
-	if(nowPlaying.length > 0 ){
-		res.end(nowPlaying);
-	}else{
-		res.end("null");
-	}
+app.get('/NowPlaying', function(req, res){
+	res.set('content-type', 'plain/text');
+		if(nowPlaying.length > 0){
+			res.end(nowPlaying);
+		}else{
+			res.end('nothing\n');
+		}
 });
 
 app.get('/clearcache', function(req, res) {
---
+++ app/routes.js
@@ -0,0 +1,57 @@
+function logfullurl(req){
+	var fullUrl = req.protocol + "://" + req.get('host') + req.originalUrl;
+	console.log("GET "+fullUrl);
+}
+
+exports.webApp = function(nowPlaying, musics, apiPath, ip){
+	return function(req, res){
+		logfullurl(req);
+		res.render('pages/index', {
+			nowPlaying: nowPlaying,
+			musics: musics,
+			apiPath: apiPath,
+			ip: ip
+		});
+	}
+}
+
+exports.pause = function(player, paused){
+	return function(req, res){
+		logfullurl(req);
+		res.set('content-type', 'plain/text');
+		if(paused){
+			player.pause();
+			res.end('Player unpaused\n');
+			paused = 0;
+		}else{
+			player.pause();
+			res.end('Player paused\n');
+			paused = 1;
+		}
+	}
+}
+
+exports.stop = function(player){
+	return function(req, res){
+		logfullurl(req);
+		res.set('content-type', 'plain/text');
+		res.end('Player stopped\n');
+		player.stop();
+	}
+}
+
+exports.mute = function(player, muted){
+	return function(req, res){
+		logfullurl(req);
+		res.set('content-type', 'plain/text');
+		if(muted){
+			player.mute();
+			res.end('Player unmuted\n');
+			muted = 0;
+		}else{
+			player.mute();
+			res.end('Player muted\n');
+			muted = 1;
+		}
+	}
+}
